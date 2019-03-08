3fe9287a9b79bfdfa7af19dc0c715aeeb6bbbe71
Sinxisx <42906722+Sinxisx@users.noreply.github.com>
2019-03-08 13:23:25 +0700

    Add files via upload
    Default, belum diubah-ubah.

---
+++ sr/robot/__init__.py
@@ -0,0 +1,13 @@
+from .simulator import Simulator
+from .sim_robot import SimRobot, AlreadyHoldingSomethingException
+from .vision import (
+    MARKER_ARENA,
+    MARKER_ROBOT,
+
+    MARKER_PEDESTAL,
+    MARKER_TOKEN,
+
+    MARKER_TOKEN_A,
+    MARKER_TOKEN_B,
+    MARKER_TOKEN_C,
+)
